7c61e9cdba4a0e47167061d686b49ac956f2d094
Your Name <your.email@example.com>
2025-10-30 12:54:55 +0530

    Update mindmic to version 2.0

--- Casks/mindmic.rb
+++ Casks/mindmic.rb
@@ -1,6 +1,6 @@
 cask "mindmic" do
   version "2.0"
-  sha256 :no_check  # Will be updated with actual SHA256 during release
+  sha256 "bc14c14202f2581758d0bb33da4ed063b78a116b72961822742a02eb22f68241"  # Will be updated with actual SHA256 during release
 
   url "https://github.com/rohit1906/mind-mic/releases/download/v#{version}/MindMic-#{version}-unsigned.dmg"
   name "MindMic"
